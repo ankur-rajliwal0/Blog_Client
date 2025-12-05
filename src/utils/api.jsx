a2bce9554723b64f00262c412708fea49392ddea
Rohit dhaka <rohitdhaka@gmail.com>
2025-12-05 15:17:34 +0530

    change baseurl

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'http://localhost:4000/api/v1',    
-  baseURL: 'https://blogserver-9ya4.onrender.com/api/v1',  
+  baseURL: 'https://blogserver1-t1yt.onrender.com/api/v1',    
+  
   
 });
 
